38aae083884f308ab7ccd78af9047dd2be63463e
Jon Russell <jon@jonrussell.xyz>
2021-02-02 10:35:39 -0600

    Added a couple tests for the User class

--- test_sql_models.py
+++ test_sql_models.py
@@ -33,4 +33,53 @@ class SymbolTestCase(TestCase):
     def tearDown(self):
         db.session.rollback()
 
+class UserTestCase(TestCase):
+    """Tests User model and it's class methods"""
+    def setUp(self):
+        User.query.delete()
+
+    def tearDown(self):
+        db.session.rollback()
+
+    def test_new_user(self):
+        # For this test, use an unhashed password, by setting it directly
+        new_user = User(username = "TEST_USER", pw_hash = "password")
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        self.assertIsNotNone(new_user.id)
     
+    def test_pw_correct_pw(self):
+        # Test the pw_hash and auth classmethods with a correct password
+        hashed_pw = User.hash_pwd("password")
+        new_user = User(username = "New_User", pw_hash = hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+
+        login_user = User.auth(new_user.username, "password")
+
+        self.assertIsInstance(login_user, User)
+
+
+    def test_pw_incorrect_pw(self):
+        # Test the pw_hash and auth classmethods with an incorrect Password
+        hashed_pw = User.hash_pwd("password")
+        new_user = User(username = "New_User", pw_hash = hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+
+        login_user = User.auth(new_user.username, "Password")
+
+        self.assertIs(login_user, False)
+
+    def test_pw_incorrect_uname(self):
+        # Test the pw_hash and auth classmethods with an incorrect Password
+        hashed_pw = User.hash_pwd("password")
+        new_user = User(username = "Correct_UserName", pw_hash = hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+
+        login_user = User.auth("Incorrect_Username", "password")
+
+        self.assertIs(login_user, False)
